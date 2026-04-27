619624872d1b6008dd302bc03f451e7a63f1f3ca
Gordon Mo <Gordon62105@gmail.com>
2026-04-26 21:28:24 -0400

    Added auth routes and setup db

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-
-app.use(express.json()); // Parse JSON bodies
-
-// Sample route
-app.get('/', (req, res) => {
-res.send('API is running...');
-});
-
-// Example API endpoint
-app.get('/api/data', (req, res) => {
-res.json({ message: 'Hello from Node.js backend!' });
-});
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
+import { connectDB } from "./config/db.js";
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/users", authRoutes);
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server started on PORT:", PORT);
+    });
+});
